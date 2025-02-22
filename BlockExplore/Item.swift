30a538a2a88a40b632c4ef52b303736544d29b22
Soso <husosong@163.com>
2025-02-22 18:17:08 +0800

    feat: add more detail for item
    Add blocknumber, blockhash, transferlog

--- BlockExplore/Item.swift
+++ BlockExplore/Item.swift
@@ -11,8 +11,13 @@ import SwiftData
 @Model
 final class Item {
     var timestamp: Date
-    
-    init(timestamp: Date) {
+    var blockNumber : String
+    var blockHash : String
+    var transferLog : String
+    init(timestamp: Date, blockNumber: String, blockHash: String) {
         self.timestamp = timestamp
+        self.blockNumber = blockNumber
+        self.blockHash = blockHash
+        self.transferLog = ""
     }
 }
